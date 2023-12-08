c2e64f65a36151c72445b7289944de905bc7206b
suyasha bobhate <suyashabobhate@gmail.com>
2023-12-07 18:48:49 -0700

    Add atb combinations

--- proj/ompabt/mm4_abt_main.c
+++ proj/ompabt/mm4_abt_main.c
@@ -12,6 +12,14 @@ void abt_par(const float *__restrict__ A, const float *__restrict__ B, float *__
 void abt_junroll_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
 void abt_kunroll_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
 void abt_alltile16_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_permute_kij_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_paralellonj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_paralellonj_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_junroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_kunroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+// void abt_kjunroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
+void abt_junrollby8(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk);
 
 void abt_seq(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk)
 {
@@ -33,10 +41,10 @@ int main(int argc, char *argv[]){
   float *A, *B, *C, *Cref;
   int i,j,k,nt,trial,max_threads,num_cases;
   int nthreads[3];
-  int Ni = 600, Nj = 600, Nk = 600;
 
-  // printf("Specify Matrix dimension Ni, Nj, Nk: ");
-  // scanf("%d %d %d", &Ni,&Nj,&Nk);
+  int Ni = atoi(argv[1]);
+  int Nj = atoi(argv[2]);
+  int Nk = atoi(argv[3]);
 
   A = (float *) malloc(sizeof(float)*Ni*Nk);
   B = (float *) malloc(sizeof(float)*Nk*Nj);
@@ -53,9 +61,9 @@ int main(int argc, char *argv[]){
   printf("Max Threads (from omp_get_max_threads) = %d\n",max_threads);
   num_cases = 3; nthreads[0]=1; nthreads[1] = max_threads/2 - 1; nthreads[2] = max_threads - 1;
 
-  for(int version=0; version<4;version++)
+  for(int version=0; version<11;version++)
   {
-    printf("Reference sequential code performance for AB (in GFLOPS)");
+    printf("Reference sequential code performance for ABT (in GFLOPS)");
     mint_seq = 1e9; maxt_seq = 0;
     for(trial=0;trial<NTrials;trial++)
     {
@@ -81,6 +89,14 @@ int main(int argc, char *argv[]){
       case 1: abt_junroll_par(A,B,C,Ni,Nj,Nk); break;
       case 2: abt_kunroll_par(A,B,C,Ni,Nj,Nk); break;
       case 3: abt_alltile16_par(A,B,C,Ni,Nj,Nk); break;
+      case 4: abt_permute_ikj_par(A,B,C,Ni,Nj,Nk); break;
+      case 5: abt_permute_kij_par(A,B,C,Ni,Nj,Nk); break;
+      case 6: abt_paralellonj_par(A,B,C,Ni,Nj,Nk); break;
+      case 7: abt_paralellonj_permute_ikj_par(A,B,C,Ni,Nj,Nk); break;
+      case 8: abt_junroll_permute_ikj_par(A,B,C,Ni,Nj,Nk); break;
+      case 9: abt_kunroll_permute_ikj_par(A,B,C,Ni,Nj,Nk); break;
+      case 10: abt_junrollby8(A,B,C,Ni,Nj,Nk); break;
+      // case 10: abt_kjunroll_permute_ikj_par(A,B,C,Ni,Nj,Nk); break;
       }
     telapsed = omp_get_wtime()-tstart;
     if (telapsed < mint_par[nt]) mint_par[nt]=telapsed;
@@ -93,6 +109,14 @@ for (int l = 0; l < Ni*Nj; l++) if (fabs((C[l] - Cref[l])/Cref[l])>threshold) {p
     case 1: printf("Performance (Best & Worst) of parallel version for abt_junroll_par (in GFLOPS)"); break;
     case 2: printf("Performance (Best & Worst) of parallel version for abt_kunroll_par (in GFLOPS)"); break;
     case 3: printf("Performance (Best & Worst) of parallel version for abt_alltile16_par (in GFLOPS)"); break;
+    case 4: printf("Performance (Best & Worst) of parallel version for abt_permute_ikj_par (in GFLOPS)"); break;
+    case 5: printf("Performance (Best & Worst) of parallel version for abt_permute_kij_par (in GFLOPS)"); break;
+    case 6: printf("Performance (Best & Worst) of parallel version for abt_paralellonj_par (in GFLOPS)"); break;
+    case 7: printf("Performance (Best & Worst) of parallel version for abt_paralellonj_permute_ikj_par (in GFLOPS)"); break;
+    case 8: printf("Performance (Best & Worst) of parallel version for abt_junroll_permute_ikj_par (in GFLOPS)"); break;
+    case 9: printf("Performance (Best & Worst) of parallel version for abt_kunroll_permute_ikj_par (in GFLOPS)"); break;
+    case 10: printf("Performance (Best & Worst) of parallel version for abt_junrollby8 (in GFLOPS)"); break;
+    // case 10: printf("Performance (Best & Worst) of parallel version for abt_kjunroll_permute_ikj_par (in GFLOPS)"); break;
     }
     for (nt=0;nt<num_cases-1;nt++) printf("%d/",nthreads[nt]);
     printf(" using %d threads\n",nthreads[num_cases-1]);
--- proj/ompabt/mm4_abt_par.c
+++ proj/ompabt/mm4_abt_par.c
@@ -17,7 +17,6 @@ void abt_par(const float *__restrict__ A, const float *__restrict__ B, float *__
   }
 }
 
-/////////// remaining to change logic from ab to abt
 ///////////////// k unrolling ///////////////////////////////////////////////
 void abt_kunroll_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
      int i, j, k;
@@ -30,13 +29,13 @@ void abt_kunroll_par(const float *__restrict__ A, const float *__restrict__ B, f
        for (j = 0; j < Nj; j++) {
           int rem = Nj % 4;
           for (k = 0; k < Nk - rem; k+=4){
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+1)]*B[(k+1)*Nj+j];
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+2)]*B[(k+2)*Nj+j];
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+3)]*B[(k+3)*Nj+j];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+1)]*B[j*Nk+(k+1)];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+2)]*B[j*Nk+(k+2)];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+3)]*B[j*Nk+(k+3)];
           }
           for (k = Nk - rem; k < Nk; k++){
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
           }
        }
      }
@@ -54,16 +53,16 @@ void abt_junroll_par(const float *__restrict__ A, const float *__restrict__ B, f
        int rem  = Ni % 4;
        for (j = 0; j < Nj - rem; j+=4) {
           for (k = 0; k < Nk; k++){
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
-               C[i*Nj+(j+1)]=C[i*Nj+(j+1)]+A[i*Nk+k]*B[k*Nj+(j+1)];
-               C[i*Nj+(j+2)]=C[i*Nj+(j+2)]+A[i*Nk+k]*B[k*Nj+(j+2)];
-               C[i*Nj+(j+3)]=C[i*Nj+(j+3)]+A[i*Nk+k]*B[k*Nj+(j+3)];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+               C[i*Nj+(j+1)]=C[i*Nj+(j+1)]+A[i*Nk+k]*B[(j+1)*Nk+k];
+               C[i*Nj+(j+2)]=C[i*Nj+(j+2)]+A[i*Nk+k]*B[(j+2)*Nk+k];
+               C[i*Nj+(j+3)]=C[i*Nj+(j+3)]+A[i*Nk+k]*B[(j+3)*Nk+k];
           }
           // C[i*Nj+j]=C[i*Nj+j]+A[i*Nj+j]*B[j*Nj+j];
        }
        for (j = Nj - rem; j < Nj; j++) {       
           for (k = 0; k < Nk; k++){
-               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
           }
        }
      }
@@ -84,7 +83,7 @@ void abt_alltile16_par(const float *__restrict__ A, const float *__restrict__ B,
                     for (i = it; i < min(it+16,Ni); i++) {
                          for (j = jt; j < min(jt+16,Nj); j++) {
                               for (k = kt; k < min(kt+16,Nk); k++){
-                                   C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
+                                   C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
                               }
                          }
                     }
@@ -94,3 +93,212 @@ void abt_alltile16_par(const float *__restrict__ A, const float *__restrict__ B,
   }
 
 }
+
+
+// permute from ijk to ikj ////////////////////////////// 
+void abt_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {
+    #pragma omp for schedule (static)
+     for (i = 0; i < Ni; i++)
+          for (k = 0; k < Nk; k++)
+               for (j = 0; j < Nj; j++)
+                   C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+
+  }
+
+}
+
+// permute from ijk to ijk ////////////////////////////// 
+void abt_permute_ijk_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {
+    #pragma omp for schedule (static)
+     for (i = 0; i < Ni; i++)
+          for (j = 0; j < Nj; j++)
+               for (k = 0; k < Nk; k++)
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+
+  }
+
+}
+
+// j unroll by 8 ////////////////////////////// 
+void abt_junrollby8(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {
+    #pragma omp for schedule (static)
+     for (i = 0; i < Ni; i++) {
+          int rem = Ni % 2;
+          for (j = 0; j < Nj; j+=2) {
+               for (k = 0; k < Nk; k++){
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+               C[i*Nj+(j+1)]=C[i*Nj+(j+1)]+A[i*Nk+k]*B[(j+1)*Nk+k];
+               // C[i*Nj+(j+2)]=C[i*Nj+(j+2)]+A[i*Nk+k]*B[(j+2)*Nk+k];
+               // C[i*Nj+(j+3)]=C[i*Nj+(j+3)]+A[i*Nk+k]*B[(j+3)*Nk+k];
+               // C[i*Nj+(j+4)]=C[i*Nj+(j+4)]+A[i*Nk+k]*B[(j+4)*Nk+k];
+               // C[i*Nj+(j+5)]=C[i*Nj+(j+5)]+A[i*Nk+k]*B[(j+5)*Nk+k];
+               // C[i*Nj+(j+6)]=C[i*Nj+(j+6)]+A[i*Nk+k]*B[(j+6)*Nk+k];
+               // C[i*Nj+(j+7)]=C[i*Nj+(j+7)]+A[i*Nk+k]*B[(j+7)*Nk+k];
+          }
+       }
+       for (j = Nj - rem; j < Nj; j++) {       
+          for (k = 0; k < Nk; k++){
+               C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+          }
+          }
+     }
+
+  }
+
+}
+
+// permute from ijk to kij ////////////////////////////// 
+void abt_permute_kij_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {
+     for (k = 0; k < Nk; k++)
+     #pragma omp for schedule (static)
+          for (i = 0; i < Ni; i++)
+               for (j = 0; j < Nj; j++)
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+
+  }
+
+}
+
+// add pragma omp for on j ////////////////////////////// 
+void abt_paralellonj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {  
+     for (i = 0; i < Ni; i++)
+          #pragma omp for schedule (static)
+          for (j = 0; j < Nj; j++)
+               for (k = 0; k < Nk; k++)
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+
+  }
+
+}
+
+// add pragma omp for on j using ikj permutation ////////////////////////////// 
+void abt_paralellonj_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {  
+     for (i = 0; i < Ni; i++)
+          for (k = 0; k < Nk; k++)
+               #pragma omp for schedule (static)
+               for (j = 0; j < Nj; j++)
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+
+  }
+
+}
+
+
+// j unroll on ikj permutation ////////////////////////////// 
+void abt_junroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {  
+     #pragma omp for schedule (static)
+     for (i = 0; i < Ni; i++) {
+          for (k = 0; k < Nk; k++) {
+               int rem = Nk % 4;
+               for (j = 0; j < Nj - rem; j+=4) {
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+                    C[i*Nj+(j+1)]=C[i*Nj+(j+1)]+A[i*Nk+k]*B[(j+1)*Nk+k];
+                    C[i*Nj+(j+2)]=C[i*Nj+(j+2)]+A[i*Nk+k]*B[(j+2)*Nk+k];
+                    C[i*Nj+(j+3)]=C[i*Nj+(j+3)]+A[i*Nk+k]*B[(j+3)*Nk+k];
+               }
+               for (j = Nj - rem; j < Nj; j++) {
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+               }
+          }  
+          
+     }
+  }
+
+}
+
+// k unroll on ikj permutation ////////////////////////////// 
+void abt_kunroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+     int i, j, k;
+
+  #pragma omp parallel private(i,j,k) 
+  {  
+     #pragma omp for schedule (static)
+     for (i = 0; i < Ni; i++) {
+          int rem = Ni % 4;
+          for (k = 0; k < Nk - rem; k+=4) {
+               for (j = 0; j < Nj; j++) {
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+1)]*B[j*Nk+(k+1)];
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+2)]*B[j*Nk+(k+2)];
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+(k+3)]*B[j*Nk+(k+3)];
+               }
+          } 
+
+          for (k = Nk - rem; k < Nk; k++) {
+               for (j = 0; j < Nj; j++) {
+                    C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[j*Nk+k];
+               } 
+          }
+          
+     }
+  }
+
+}
+
+// k and j unroll on ikj permutation ////////////////////////////// 
+// void abt_kjunroll_permute_ikj_par(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C, int Ni, int Nj, int Nk) {
+//      int i, j, k;
+
+//   #pragma omp parallel private(i,j,k) 
+//   {  
+//      #pragma omp for schedule (static)
+//      for (i = 0; i < Ni; i++) {
+//           for (k = 0; k < Nk; k+=4) {
+//                for (j = 0; j < Nj; j+=4) {
+//                     C[i*Nj+j]=C[i*Nj+j]+A[i*Nk+k]*B[k*Nj+j];
+//                     C[i*Nj+(j+1)]=C[i*Nj+(j+1)]+A[i*Nk+k]*B[k*Nj+(j+1)];
+//                     C[i*Nj+(j+2)]=C[i*Nj+(j+2)]+A[i*Nk+k]*B[k*Nj+(j+2)];
+//                     C[i*Nj+(j+3)]=C[i*Nj+(j+3)]+A[i*Nk+k]*B[k*Nj+(j+3)];
+
+//                     C[i*Nj+(j+4)]=C[i*Nj+(j+4)]+A[i*Nk+(k+1)]*B[(k+1)*Nj+j];
+//                     C[i*Nj+(j+5)]=C[i*Nj+(j+5)]+A[i*Nk+(k+1)]*B[(k+1)*Nj+(j+1)];
+//                     C[i*Nj+(j+6)]=C[i*Nj+(j+6)]+A[i*Nk+(k+1)]*B[(k+1)*Nj+(j+2)];
+//                     C[i*Nj+(j+7)]=C[i*Nj+(j+7)]+A[i*Nk+(k+1)]*B[(k+1)*Nj+(j+3)];
+
+//                     C[i*Nj+(j+8)]=C[i*Nj+(j+8)]+A[i*Nk+(k+2)]*B[(k+2)*Nj+j];
+//                     C[i*Nj+(j+9)]=C[i*Nj+(j+9)]+A[i*Nk+(k+2)]*B[(k+2)*Nj+(j+1)];
+//                     C[i*Nj+(j+10)]=C[i*Nj+(j+10)]+A[i*Nk+(k+2)]*B[(k+2)*Nj+(j+2)];
+//                     C[i*Nj+(j+11)]=C[i*Nj+(j+11)]+A[i*Nk+(k+2)]*B[(k+2)*Nj+(j+3)];
+
+//                     C[i*Nj+(j+12)]=C[i*Nj+(j+12)]+A[i*Nk+(k+3)]*B[(k+3)*Nj+j];
+//                     C[i*Nj+(j+13)]=C[i*Nj+(j+13)]+A[i*Nk+(k+3)]*B[(k+3)*Nj+(j+1)];
+//                     C[i*Nj+(j+14)]=C[i*Nj+(j+14)]+A[i*Nk+(k+3)]*B[(k+3)*Nj+(j+2)];
+//                     C[i*Nj+(j+15)]=C[i*Nj+(j+15)]+A[i*Nk+(k+3)]*B[(k+3)*Nj+(j+3)];
+//                }
+               
+              
+//           } 
+          
+//      }
+//   }
+
+// }
+
