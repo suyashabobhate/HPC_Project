19dadedae7d8ebb488f83757490a1533ae5eb5fb
suyasha bobhate <suyashabobhate@gmail.com>
2023-12-11 19:12:16 -0700

    Update script and add output

--- proj/Openmp/mm4_lonepeak_cp.bat
+++ proj/Openmp/mm4_lonepeak_cp.bat
@@ -11,5 +11,5 @@
 echo "*** Assigned Lonepeak Node: " $SLURMD_NODENAME | tee -a lonepeak_sym.$SLURM_JOB_ID\.log
 echo " " | tee -a lonepeak_sym.$SLURM_JOB_ID\.log
 echo "Par-No-UNR " | tee -a lonepeak_sym.$SLURM_JOB_ID\.log
-gcc -O3 -fopenmp -o sym main.c
+gcc -O3 -fopenmp -o sym original\ files/main.c
 ./sym | tee -a lonepeak_sym.$SLURM_JOB_ID\.log
